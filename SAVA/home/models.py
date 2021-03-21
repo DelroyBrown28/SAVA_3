183766d3a65962f3842bc0b82a017b32acebe0ce
Delroy Brown <delroybrown.db@gmail.com>
2021-03-21 20:45:48 +0000

    Started homepage styling

--- SAVA/home/models.py
+++ SAVA/home/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    """This will be the main page"""
+    template = 'home/home_page.html'
+    max_count = 1
+
+    main_page_title = models.CharField(max_length=400, blank=False, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("main_page_title")
+    ]
+
+    class Meta:
+
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
