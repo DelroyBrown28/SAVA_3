454e35e170611feb3bb153c32340d4b87ec65aeb
Delroy Brown <delroybrown.db@gmail.com>
2021-03-26 02:12:34 +0000

    Added unstyles modal to contact page displaying contact help info

--- SAVA/SAVA/static/js/SAVA.js
+++ SAVA/SAVA/static/js/SAVA.js
@@ -1,63 +1,47 @@
 AOS.init();
 
-var RecaptchaOptions = {
-    theme: 'custom',
-    custom_theme_widget: 'recaptcha_widget'
-};
-
-
-
-const showServices = document.querySelector('.services-btn');
-const showServices2 = document.querySelector('.btn-to-services');
-const closeServices = document.querySelector('.close-btn');
-const navOpen = document.querySelector('nav-open');
+const showHelpInfo = document.querySelector('.show-contact-help-btn');
+const closeHelpInfo = document.querySelector('.help-info-close-btn');
 
 
 // Stops animation from playing unless refreshed
-const tl = new TimelineLite({
+const newtl = new TimelineLite({
     paused: true,
     reversed: true
 });
 
-tl.to('.remove-me', 0.3, {
-        autoAlpha: 0,
-        stagger: 0.1,
-    })
-    .to('.services-section', 0.3, {
+newtl.to('.contact-help-info-modal', 0.3, {
         autoAlpha: 1,
+        height: '100%',
+        width: '65%',
+        top: '0px',
+        borderRadius: '0px',
     })
 
 
-// Button to toggle animation
-showServices.addEventListener('click', () => {
+// Button to close services sectio
+showHelpInfo.addEventListener('click', () => {
 
-    if (tl.isActive()) {
+    if (newtl.isActive()) {
         e.preventDefault();
         e.stopImmediatePropagation();
         return false;
     }
-    toggleTween(tl)
+    toggleTween(newtl)
 })
-showServices2.addEventListener('click', () => {
 
-    if (tl.isActive()) {
+
+
+closeHelpInfo.addEventListener('click', () => {
+
+    if (newtl.isActive()) {
         e.preventDefault();
         e.stopImmediatePropagation();
         return false;
     }
-    toggleTween(tl)
+    toggleTween(newtl)
 })
 
-
-closeServices.addEventListener('click', () => {
-
-    if (tl.isActive()) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        return false;
-    }
-    toggleTween(tl)
-})
 
 function toggleTween(tween) {
     tween.reversed() ? tween.play() : tween.reverse();
