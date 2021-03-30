053d483faf8fd3007c1b8bdfe408f518603b292f
Delroy Brown <delroybrown.db@gmail.com>
2021-03-30 14:09:15 +0100

    Setting up for deployment

--- SAVA/SAVA/settings/dev.py
+++ SAVA/SAVA/settings/dev.py
@@ -1,13 +1,16 @@
 from .base import *
+import os
+
+env = os.environ.copy()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4xdgjg3@umnk^--qan3+vupq970$wh*f(u568ta84bma8y0chk'
+SECRET_KEY = 'JvJhZBaDbVjqQSZOnUWfjRplI6XQtXs4GnfeOjN1qb4CDFQ4rFCVrBWkrbib'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*',  '127.0.0.1'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
